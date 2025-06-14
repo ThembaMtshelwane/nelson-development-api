8faf84cd06c11459686dbf6e8d138b7d567d25cb
ThembaMtshelwane <thembamm3@gmail.com>
2025-06-14 14:52:29 +0200

    Post: Take a word and arrange the charaters alphabetically in an array

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,35 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
+
 import { PORT } from "../src/constants/env.const";
+import { Data } from "../src/types";
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welocme to the api");
 });
+
+app.post("/", (req: Request, res: Response) => {
+  const data: Data = req.body;
+  const word: string = data.data;
+  const letterArray: string[] = word.split("");
+  const orderedLetters: string[] = letterArray.sort();
+
+  if (orderedLetters.length) {
+    res.status(200).json({
+      word: orderedLetters,
+    });
+  } else {
+    res.status(500).json({
+      message: "Error: No ordered letters",
+    });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server running: http://localhost:${PORT}`);
 });
